520c118979d3501d358b060b261022a9481f565d
lazinus1 <lazinus@tumo.world>
2023-11-27 17:44:22 +0100

    Fehler "Funktioniert mit den Filtern nicht!"

--- game/gras.js
+++ game/gras.js
@@ -16,6 +16,7 @@ class Gras extends livingCreature{
     spielzug() {
         if (this.energie > 4) {
             this.energie = 0;
+            // this.pflanzNeuesObjekt(1, this.istErde);
             this.pflanzNeuesGrasObjekt();
             // mach etwas
         } else {
@@ -25,36 +26,37 @@ class Gras extends livingCreature{
     };
 
     pflanzNeuesGrasObjekt() {
-        let erdeFelder = this.erstelleErdefelderTabelle();
+        // let erdeFelder = this.erstelleErdefelderTabelle(this.istErde); 
+        let erdeFelder = this.umgebung.filter(this.istFeld());
         if (erdeFelder.length > 0) {
             let gewähltesFeld = erdeFelder[0];
             let neuesGrasObjekt = new Gras(gewähltesFeld[0],gewähltesFeld[1]);
-            neuesGrasObjekt.platziereSelbstInMatrix();
+            neuesGrasObjekt.platziereSelbstInMatrix(1);
             objekteListe.push(neuesGrasObjekt);
         }
     };
 
-    erstelleErdefelderTabelle() {
-        let benachbarteFelder = [
-            [this.zeile+1,this.spalte],
-            [this.zeile-1,this.spalte],
-            [this.zeile,this.spalte+1],
-            [this.zeile,this.spalte-1],
-        ]
-        return benachbarteFelder.filter(this.istErde);
-    };
-    istErde(koordinatenPaar) {
-        let zeile = koordinatenPaar[0];
-        let spalte = koordinatenPaar[1];
-        if (zeile >= 0
-            && spalte >= 0
-            && zeile < matrix.length
-            && spalte < matrix.length
-            && matrix[zeile][spalte] === 0
-        ) {
-            return true;
-        }  else {
-            return false;
-        }
-    };
+    // erstelleErdefelderTabelle() {
+    //     let benachbarteFelder = [
+    //         [this.zeile+1,this.spalte],
+    //         [this.zeile-1,this.spalte],
+    //         [this.zeile,this.spalte+1],
+    //         [this.zeile,this.spalte-1],
+    //     ]
+    //     return benachbarteFelder.filter(this.istErde);
+    // };
+    // istErde(koordinatenPaar) {
+    //     let zeile = koordinatenPaar[0];
+    //     let spalte = koordinatenPaar[1];
+    //     if (zeile >= 0
+    //         && spalte >= 0
+    //         && zeile < matrix.length
+    //         && spalte < matrix.length
+    //         && matrix[zeile][spalte] === 0
+    //     ) {
+    //         return true;
+    //     }  else {
+    //         return false;
+    //     }
+    // };
 }
--- game/livingCreature.js
+++ game/livingCreature.js
@@ -3,5 +3,31 @@ class livingCreature {
     constructor(zeile, spalte) {
         this.zeile = zeile;
         this.spalte = spalte;
+
+        this.umgebung = [
+            [this.zeile - 1, this.y - 1],
+            [this.zeile, this.spalte - 1],
+            [this.zeile + 1, this.spalte + 1],
+            [this.zeile - 1, this.spalte],
+            [this.zeile + 1, this.spalte],
+            [this.zeile - 1, this.spalte + 1],
+            [this.zeile, this.spalte + 1],
+            [this.zeile + 1, this.spalte +1]
+        ]
+    }
+
+    istFeld(koordinatenPaar) {
+        let zeile = koordinatenPaar[0];
+        let spalte = koordinatenPaar[1];
+        if (zeile >= 0
+            && spalte >= 0
+            && zeile < matrix.length
+            && spalte < matrix.length
+            && matrix[zeile][spalte] === 0
+        ) {
+            return true;
+        }  else {
+            return false;
+        }
     }
 }
